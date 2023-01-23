989722fe1837aaaedd6ab956f242333c89086006
Fedomus <federico.liousin@gmail.com>
2023-01-22 23:03:49 -0300

    third section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FirstsectionComponent } from './firstsection/firstsection.component';
+import { SecondsectionComponent } from './secondsection/secondsection.component';
+import { ThirdsectionComponent } from './thirdsection/thirdsection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FirstsectionComponent
+    FirstsectionComponent,
+    SecondsectionComponent,
+    ThirdsectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/secondsection/secondsection.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SecondsectionComponent } from './secondsection.component';
+
+describe('SecondsectionComponent', () => {
+  let component: SecondsectionComponent;
+  let fixture: ComponentFixture<SecondsectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SecondsectionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SecondsectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
